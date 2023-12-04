56045cd3d1756682760f91f451c6a8e5997295d4
Caio Ramos de Lima <ramoscaio69@gmail.com>
2023-12-04 19:09:30 -0300

    04-12-2023

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,3 +1,4 @@
+/* import { AppRoutingModule } from './app.routing.module'; */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MedicoService } from 'src/service/MedicoService';
@@ -9,7 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule} from '@angular/material/toolbar'; 
-import { MatIcon, MatIconModule} from '@angular/material/icon'; 
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule} from '@angular/material/list'; 
 import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
